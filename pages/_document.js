33bc6f93a47477f7b1b5d9244565f8f19be3ce38
iyaiyayo26 <fahiyanmr26@gmail.com>
2023-01-04 16:45:14 +0700

    first commit

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      {/* <Head /> */}
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Poppins:wght@400;600&display=swap" rel="stylesheet"></link>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        Violet: "hsl(257, 40%, 49%)",
+        "Soft-Magenta": "hsl(300, 69%, 71%)",
+      },
+      fontFamily: {
+        Poppins: ["Poppins", "sans-serif"],
+        "Open-sans": ["Open Sans", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
